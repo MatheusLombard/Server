b7d5013e655953bed676444f9b4fa3e7d9baf7ab
Matheus <matheus.profissional2022@gmail.com>
2024-09-25 20:11:11 -0300

    esqueceram de mim

---
+++ conexao.js
@@ -0,0 +1,12 @@
+const sequelize = new Sequelize('emergencialsaude', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+}) 
+
+sequelize.authenticate().then(() => {
+    console.log('Conectado com sucesso')
+}).catch((error) => {
+    console.log('Problemas ao se comunicar com o BD: ', error)
+})
+
+module.exports = sequelize;
